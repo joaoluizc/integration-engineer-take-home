bb2b3e724df3833d76ae87c7073031b38dd14a3a
joaoluizc <joaocoelho.tdm@gmail.com>
2024-07-31 23:17:18 -0300

    chore: implement creation and deletion of tasks

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.use(bodyParser.json());
+app.use(cors());
 
 let tasks = [];
 let nextTaskId = 1;
@@ -14,10 +16,33 @@ app.get('/tasks', (req, res) => {
 });
 
 app.post('/tasks', (req, res) => {
+  const { title, description } = req.body;
+
+  if (!title) {
+    return res.status(400).json({'message': 'title is required'});
+  }
+
+  if (!description) {
+    return res.status(400).json({'message': 'description is required'});
+  }
+
+  tasks.push({
+    id: nextTaskId,
+    title: req.body.title,
+    description: req.body.description,
+  });
+  nextTaskId += 1;
+  res.status(201).json();
 });
 
 
 app.delete('/tasks/:id', (req, res) => {
+  const task = tasks.find(task => task.id === parseInt(req.params.id));
+  if (!task) {
+    return res.status(400).json({'message': 'task not found'});
+  }
+  tasks = tasks.filter(task => task.id !== parseInt(req.params.id));
+  res.json(tasks);
 });
 
 app.listen(PORT, () => {
